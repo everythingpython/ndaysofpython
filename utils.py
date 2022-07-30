222bf23ac517e8c63e5f80d8cb0d8f3fbdfe91be
Abhiram R <abhi.darkness@gmail.com>
2022-07-31 02:09:40 +0530

    Added Day02 code

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from time import time
+import os
 
 def timing(f):
     @wraps(f)
@@ -7,7 +8,7 @@ def timing(f):
         ts = time()
         result = f(*args, **kw)
         te = time()
-        print(f'func:{f.__name__} args:[{args}, {kw}] took: {(te-ts):2.4f} sec\n')
+        print(f'func:{f.__name__} took: {(te-ts):2.4f} sec\n')
         return result
     return wrap
 
@@ -16,3 +17,8 @@ def sqrt_num(n):
     return n, n**0.5
     
     
+def get_file_size(filename):
+    file_size = os.path.getsize(filename)
+    file_size = file_size/(1024.0*1024.0)
+    file_size = round(file_size,2)
+    return file_size
